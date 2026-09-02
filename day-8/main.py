00c9639d91e370e7ba4974e225dd5433de916317
Dhawal Tete <dhawaldk42@gmail.com>
2026-09-02 17:02:29 +0530

    craeted day 8 folder and learning fast api get_post_put_delete

---
+++ day-8/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from models import Asset
+from fastapi import HTTPException
+app = FastAPI()
+
+assets = [
+    {
+        "assetnum": "PUMP100",
+        "description": "Water Pump",
+        "status": "ACTIVE"
+    }
+]
+
+@app.get("/assets")
+def get_assets():
+    return assets
+
+@app.post("/assets")
+def add_asset(asset:Asset):
+    assets.append(asset.model_dump())
+    print(assets)
+    return {"Message":"Asset added",
+            "Asset":asset}
+
+@app.get("/asset/{assetnum}")
+def get_asset(assetnum:str):
+    for asset in assets:
+        if asset['assetnum'] == assetnum:
+            return asset
+    raise HTTPException(status_code=404,detail="Asset not found")
+
+@app.put("/asset/{assetnum}")
+def update_asset(assetnum:str,updated_asset:Asset):
+    for index,asset in enumerate(assets):
+        if asset['assetnum'] == assetnum:
+            assets[index] = updated_asset.model_dump()
+            return {"Message":"Asset updated"}
+    return{"Message":"Asset not found"}
+
+@app.delete("/asset/{assetnum}")
+def delete_asset(assetnum):
+    for asset in assets:
+        if asset['assetnum'] == assetnum:
+            assets.remove(asset)
+            return {"Message":"asset deleted"}
+    raise HTTPException(status_code=404,detail="Asset not found")
